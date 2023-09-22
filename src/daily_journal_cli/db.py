327658c447489f63bdef23c8136a07e77a127ec6
Nick Roberts <nickroberts404@gmail.com>
2023-09-21 22:44:34 -0500

    added create/retrieive methods for entries table

--- src/daily_journal_cli/db.py
+++ src/daily_journal_cli/db.py
@@ -1,5 +1,5 @@
 import sqlite3
-from datetime import datetime
+from datetime import date, datetime
 
 cx = sqlite3.connect("daily-journal.db")
 
@@ -21,21 +21,31 @@ def create_tables():
         )
 
 
-def get_entries_for_date():
-    raise NotImplemented()
-
-
-def insert_entry(content: str, date: datetime):
-    raise NotImplemented()
-
-
-def test():
+def get_entries_for_date(date: date):
     with cx:
         cu = cx.cursor()
-        data = cu.execute("SELECT * FROM entries")
-        print(data)
+        cu.execute("SELECT * FROM entries WHERE date = ?", (date,))
+        rows = cu.fetchall()
+        print(rows)
+
+
+def insert_entry(content: str, date: date):
+    now = datetime.now()
+    with cx:
+        cu = cx.cursor()
+        row = (content, date, now, now)
+        cu.execute(
+            "INSERT INTO entries (content, date, created_at, updated_at) VALUES(?, ?, ?, ?)",
+            row,
+        )
+
+
+def populate_data():
+    now = date.today()
+    records = [("test 1", now), ("test 2", now), ("test 3", now)]
+    for r in records:
+        insert_entry(*r)
 
 
 def setup():
     create_tables()
-    test()
