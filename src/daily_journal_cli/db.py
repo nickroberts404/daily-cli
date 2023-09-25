096b4469d863eaf3d85cbfb68b6007829053e9f7
Nick Roberts <nickroberts404@gmail.com>
2023-09-25 14:02:28 -0500

    moved db to class; created Entry class; entries are persisted

--- src/daily_journal_cli/db.py
+++ src/daily_journal_cli/db.py
@@ -4,48 +4,72 @@ from datetime import date, datetime
 cx = sqlite3.connect("daily-journal.db")
 
 
-def create_tables():
-    with cx:
-        cu = cx.cursor()
-        cu.execute("DROP TABLE IF EXISTS entries;")
-        cu.execute(
-            """
-                CREATE TABLE IF NOT EXISTS entries (
-                   id INTEGER PRIMARY KEY,
-                   content TEXT NOT NULL,
-                   date TEXT NOT NULL,
-                   created_at TEXT NOT NULL,
-                   updated_at TEXT NOT NULL
-                );
-            """
-        )
+class Entry:
+    def __init__(self, raw_entry):
+        print(raw_entry)
+        self.id = raw_entry[0]
+        self.content = raw_entry[1]
+        self.date = raw_entry[2]
+        self.created_at = raw_entry[3]
+        self.updated_at = raw_entry[4]
 
 
-def get_entries_for_date(date: date):
-    with cx:
-        cu = cx.cursor()
-        cu.execute("SELECT * FROM entries WHERE date = ?", (date,))
-        rows = cu.fetchall()
-        print(rows)
+class DB:
+    cx = sqlite3.connect("daily-journal.db")
 
+    def __init__(self):
+        self.createEntriesTable()
 
-def insert_entry(content: str, date: date):
-    now = datetime.now()
-    with cx:
-        cu = cx.cursor()
+    def createEntriesTable(self):
+        """Create Entries table in DB"""
+        with cx:
+            cu = cx.cursor()
+            # cu.execute("DROP TABLE IF EXISTS entries;")
+            cu.execute(
+                """
+                    CREATE TABLE IF NOT EXISTS entries (
+                    id INTEGER PRIMARY KEY,
+                    content TEXT NOT NULL,
+                    date TEXT NOT NULL,
+                    created_at TEXT NOT NULL,
+                    updated_at TEXT NOT NULL
+                    );
+                """
+            )
+
+    def get_entries_by_date(self, date: date) -> list[Entry]:
+        with cx:
+            cu = cx.cursor()
+            cu.execute("SELECT * FROM entries WHERE date = ?", (date,))
+            rows = cu.fetchall()
+        return [Entry(row) for row in rows]
+
+    def get_all_entries(self) -> list[Entry]:
+        with cx:
+            cu = cx.cursor()
+            cu.execute("SELECT * FROM entries;")
+            rows = cu.fetchall()
+        return [Entry(row) for row in rows]
+
+    def insert_entry(self, content: str, date: date) -> int:
+        now = datetime.now()
         row = (content, date, now, now)
-        cu.execute(
-            "INSERT INTO entries (content, date, created_at, updated_at) VALUES(?, ?, ?, ?)",
-            row,
-        )
+        with cx:
+            cu = cx.cursor()
+            cu.execute(
+                "INSERT INTO entries (content, date, created_at, updated_at) VALUES(?, ?, ?, ?);",
+                row,
+            )
+            cu.execute("SELECT last_insert_rowid();")
+            rowid = cu.fetchone()
+        return rowid
+
+
+database = DB()
 
 
 def populate_data():
     now = date.today()
     records = [("test 1", now), ("test 2", now), ("test 3", now)]
     for r in records:
-        insert_entry(*r)
-
-
-def setup():
-    create_tables()
+        database.insert_entry(*r)
--- src/daily_journal_cli/main.py
+++ src/daily_journal_cli/main.py
@@ -1,22 +1,29 @@
-import typer
-import datetime
-from rich import print
-from rich.prompt import Prompt
+from datetime import datetime, date
 from typing_extensions import Annotated
 
-existing_entries = ["Ate breakfast", "Went on a run with Lauren"]
-today = datetime.date.today()
+import typer
+from rich import print
+from rich.prompt import Prompt
+
+from .db import DB, Entry
+
+database = DB()
+today = date.today()
 
 
-def format_date(date):
+def format_date(date: date) -> str:
     return date.strftime("%b %d, %Y")
 
 
-def get_target_date(date):
+def string_to_date(date_string: str) -> date:
+    return datetime.strptime(date_string, "%m/%d/%Y").date()
+
+
+def get_target_date(date_string: str or None) -> date:  # IS this a date or datetime
     target_date = today
-    if date:
+    if date_string:
         try:
-            target_date = datetime.datetime.strptime(date, "%m/%d/%Y")
+            target_date = string_to_date(date_string)
         except:
             print(
                 "[red bold]Please provide date in valid format[/] (example: 05/26/1994)."
@@ -25,16 +32,16 @@ def get_target_date(date):
     return target_date
 
 
-def print_collect_entries_header(date):
+def print_collect_entries_header(date: date):
     formatted_date = format_date(date)
     print(
         f"[bold blue]What did you do today [not bold italic]({formatted_date})[/]?[/]"
     )
 
 
-def print_entry_list(entries):
+def print_entry_list(entries: list[Entry]):
     for entry in entries:
-        print(f"[bold blue]*[/] {entry}")
+        print(f"[bold blue]*[/] {entry.content} [dim]({entry.id})[/]")
 
 
 def prompt_user_for_entries():
@@ -46,12 +53,12 @@ def prompt_user_for_entries():
             yield input
 
 
-def collect_entries(date):
-    entries = []
+def collect_entries(date: date):
+    existing_entries = database.get_entries_by_date(date)
     print_collect_entries_header(date)
     print_entry_list(existing_entries)
     for entry in prompt_user_for_entries():
-        entries.append(entry)
+        database.insert_entry(entry, date)
         # TODO Add entries to DB
 
 
